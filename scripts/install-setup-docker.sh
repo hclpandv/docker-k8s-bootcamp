36f95a5c9f66d61989d607d7a83ab1249067c2f8
Vikas Pandey <vikaspa6@gmail.com>
2021-07-15 14:52:10 +0800

    Update install-setup-docker.sh

--- scripts/install-setup-docker.sh
+++ scripts/install-setup-docker.sh
@@ -12,8 +12,10 @@ sudo apt-get install docker.io -y
 
 # Install dns resolver and add DNS entries
 sudo apt install resolvconf -y
-echo "nameserver 8.8.4.4" >> /etc/resolvconf/resolv.conf.d/head
-echo "nameserver 8.8.8.8" >> /etc/resolvconf/resolv.conf.d/head
+echo 'nameserver 8.8.4.4' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+echo 'nameserver 8.8.8.8' | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+
+sudo service resolvconf restart
 
 # Install docker-compose and get execuatble access
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
